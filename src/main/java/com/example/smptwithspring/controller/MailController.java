ccf0892e4a0c1cb4832ce272f3ca1680e719b0b2
rumeshrath97 <g9673.rumesh@gmail.com>
2023-02-20 01:01:23 +0530

    Create Post method to make email and send it to selected email address

---
+++ src/main/java/com/example/smptwithspring/controller/MailController.java
@@ -0,0 +1,38 @@
+package com.example.smptwithspring.controller;
+
+import com.example.smptwithspring.DTO.MailControllerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/mail")
+public class MailController {
+
+    /*Plugged Java Mail Sender*/
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    /*Create a post method to send email and create DTO to import DTO class*/
+    @PostMapping("/send")
+    public String sendEmail(@RequestBody MailControllerDTO mailControllerDTO){
+        try{
+            SimpleMailMessage message=new SimpleMailMessage();
+            message.setSubject(mailControllerDTO.getSubject());
+            message.setFrom("g9673.rumesh@gmail.com");
+            message.setTo(mailControllerDTO.getTomMail());
+            message.setText(mailControllerDTO.getMessage());
+
+            javaMailSender.send(message);
+            return "Success!";
+
+        } catch (Exception e){
+            return e.getMessage();
+        }
+
+    }
+}
