caba1a19d8ef743c6147a3480b6544d035c6f1bd
rumeshrath97 <g9673.rumesh@gmail.com>
2023-02-20 00:56:49 +0530

    first commit

---
+++ src/main/java/com/example/smptwithspring/SmptWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.smptwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmptWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmptWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smptwithspring/SmptWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smptwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmptWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
